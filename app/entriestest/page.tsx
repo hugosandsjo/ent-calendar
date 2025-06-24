d2e7f3a21845692557836880eab7fcc9cef238ae
hugosandsjo <hugosandsjo@gmail.com>
2025-06-24 17:14:20 +0200

    Remove comments from page.tsx to clean up code and improve readability

--- app/entriestest/page.tsx
+++ app/entriestest/page.tsx
@@ -1,22 +1,22 @@
-// "use client";
+"use client";
 
-// import { createClient } from "@/lib/supabase/client";
-// import { useEffect, useState } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { useEffect, useState } from "react";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// export default function Page() {
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   const [notes, setNotes] = useState<any[] | null>(null);
-//   const supabase = createClient();
+export default function Page() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [notes, setNotes] = useState<any[] | null>(null);
+  const supabase = createClient();
 
-//   useEffect(() => {
-//     const getData = async () => {
-//       const { data } = await supabase.from("entries").select();
-//       setNotes(data);
-//     };
-//     getData();
-//   }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from("entries").select();
+      setNotes(data);
+    };
+    getData();
+  }, []);
 
-//   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
-// }
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
