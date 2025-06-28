ea5bc23bbb09456f3216125ee2dded7c3fdd245e
hugosandsjo <hugosandsjo@gmail.com>
2025-06-28 12:24:01 +0200

    feat: implement dynamic dashboard page with create, edit, and detail views; remove obsolete pages

--- src/app/dashboard/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import EntryForm from "@/src/app/components/form/EntryForm";
-
-export default function Create() {
-  return (
-    <section className="flex flex-col w-screen items-center justify-center mb-12">
-      <EntryForm />
-    </section>
-  );
-}
