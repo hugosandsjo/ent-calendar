c848747e4b084a52b3d427f796252d7c97c732e4
hugosandsjo <hugosandsjo@gmail.com>
2025-06-30 11:35:47 +0200

    refactor: enhance Entry detail layout for better user experience and responsiveness

--- src/app/dashboard/[...id]/page.tsx
+++ src/app/dashboard/[...id]/page.tsx
@@ -66,50 +66,53 @@ export default function DynamicDashboardPage({ params }: PageProps) {
     return (
       <section className="flex justify-center py-14">
         <div className="w-10/12 px-10 py-12 flex flex-col gap-4 bg-brand-movie rounded-xl">
-          <div className="flex justify-between flex-col lg:flex-row">
+          <div className="flex flex-col lg:flex-row">
             <Link href="/dashboard" className="hover:opacity-60">
               <ArrowLeftIcon className="w-8 h-8 mb-4" />
             </Link>
-            <h1 className="text-5xl leading-tight">{entry.title}</h1>
-            <div></div>
-          </div>
-          {entry ? (
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-x-2 mb-2 text-1xl flex-wrap gap-y-2">
-                <h2 className="bg-white rounded-3xl py-1 px-3">
-                  {entry.category}
-                </h2>
-                {entry.author ? (
-                  <h2 className="bg-white rounded-3xl py-1 px-3">
-                    {entry.author}
-                  </h2>
-                ) : null}
-                {entry.director ? (
-                  <h2 className="border border-black bg-white rounded-3xl py-1 px-3">
-                    {entry.director}
-                  </h2>
-                ) : null}
-                <h2 className="rounded-3xl bg-white py-1 px-3">{entry.year}</h2>
+            <div className="flex flex-col gap-4 lg:px-20 justify-center ">
+              <h1 className="text-5xl leading-tight">{entry.title}</h1>
+              {entry ? (
+                <div className="flex flex-col gap-2">
+                  <div className="flex gap-x-2 mb-2 text-1xl flex-wrap gap-y-2">
+                    <h2 className="bg-white rounded-3xl py-1 px-3">
+                      {entry.category}
+                    </h2>
+                    {entry.author ? (
+                      <h2 className="bg-white rounded-3xl py-1 px-3">
+                        {entry.author}
+                      </h2>
+                    ) : null}
+                    {entry.director ? (
+                      <h2 className="bg-white rounded-3xl py-1 px-3">
+                        {entry.director}
+                      </h2>
+                    ) : null}
+                    <h2 className="rounded-3xl bg-white py-1 px-3">
+                      {entry.year}
+                    </h2>
+                  </div>
+                  <RatingTag rating={entry.rating ?? undefined} />
+                  <p className="text-lg">{entry.description}</p>
+                </div>
+              ) : (
+                <p>Loading...</p>
+              )}
+              <div className="flex gap-4">
+                <Link href={`/dashboard/${entryId}/edit`}>
+                  <button className="hover:bg-sky-300 py-2 rounded-xl">
+                    <EditIcon className="w-6 h-6 inline-block" />
+                  </button>
+                </Link>
+                <div className="flex gap-x-2">
+                  <button
+                    className="hover:bg-red-400 py-2 rounded-xl"
+                    onClick={() => handleDeleteClick(Number(entryId))}
+                  >
+                    <TrashIcon className="w-6 h-6 inline-block" />
+                  </button>
+                </div>
               </div>
-              <RatingTag rating={entry.rating ?? undefined} />
-              <p className="text-lg">{entry.description}</p>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
-          <div className="flex mt-4 lg:justify-end">
-            <Link href={`/dashboard/${entryId}/edit`}>
-              <button className="hover:bg-sky-300 py-2 px-3 rounded-xl">
-                <EditIcon className="w-6 h-6 inline-block" />
-              </button>
-            </Link>
-            <div className="flex gap-x-2">
-              <button
-                className="hover:bg-red-400 py-2 px-3 rounded-xl"
-                onClick={() => handleDeleteClick(Number(entryId))}
-              >
-                <TrashIcon className="w-6 h-6 inline-block" />
-              </button>
             </div>
           </div>
         </div>
