a076c98fdc24300144a64da3559fedde3b3cc4dd
hugosandsjo <hugosandsjo@gmail.com>
2025-08-02 08:50:08 +0200

    refactor: implement delete confirmation dialog in DetailPage component

--- src/app/components/ui/DetailPage.tsx
+++ src/app/components/ui/DetailPage.tsx
@@ -6,6 +6,7 @@ import { ArrowLeftIcon, TrashIcon, EditIcon } from "@/src/app/components/Icons";
 import RatingTag from "@/src/app/components/form/RatingTag";
 import { useRouter } from "next/navigation";
 import { deleteEntry } from "@/src/app/actions/actions";
+import { useState } from "react";
 
 type DetailPageProps = {
   entry: SelectEntry;
@@ -14,13 +15,38 @@ type DetailPageProps = {
 
 export default function DetailPage({ entry, entryId }: DetailPageProps) {
   const router = useRouter();
-  const handleDeleteClick = async (id: number) => {
+  const [toggleDelete, setToggleDelete] = useState(false);
+
+  const handleDeleteEntry = async (id: number) => {
     await deleteEntry(id);
     router.push("/dashboard");
   };
 
   return (
     <div className="flex flex-col max-w-[64rem]">
+      {toggleDelete ? (
+        <section className="absolute flex items-center justify-center top-0 bottom-0 right-0 left-0">
+          <div className="bg-pink-200 flex flex-col gap-4 p-20 relative">
+            <h1>Are you sure you want to delete this entry?</h1>
+            <div className="flex gap-2">
+              <button
+                className="border border-black p-6"
+                onClick={() => handleDeleteEntry(Number(entryId))}
+              >
+                Delete
+              </button>
+              <button
+                className="py-2 px-4 absolute top-5 right-5 bg-black text-white"
+                onClick={() => {
+                  setToggleDelete(false);
+                }}
+              >
+                x
+              </button>
+            </div>
+          </div>
+        </section>
+      ) : null}
       <div className="flex justify-between mb-4">
         <div className="flex items-center justify-center">
           <Link href="/dashboard" className="hover:opacity-60">
@@ -29,7 +55,7 @@ export default function DetailPage({ entry, entryId }: DetailPageProps) {
         </div>
       </div>
       <div className="px-10 py-8 md:py-12 flex flex-col gap-4 bg-brand-movie rounded-xl">
-        <div className="flex flex-col lg:flex-row ">
+        <div className="flex flex-col lg:flex-row">
           <div className="flex flex-col gap-4 lg:px-10 justify-center">
             <h1 className="text-5xl leading-tight">{entry.title}</h1>
             {entry ? (
@@ -67,7 +93,7 @@ export default function DetailPage({ entry, entryId }: DetailPageProps) {
               <div className="flex gap-x-2">
                 <button
                   className="hover:bg-red-400 py-2 rounded-xl px-2"
-                  onClick={() => handleDeleteClick(Number(entryId))}
+                  onClick={() => setToggleDelete(!toggleDelete)}
                 >
                   <TrashIcon className="w-6 h-6 inline-block" />
                 </button>
